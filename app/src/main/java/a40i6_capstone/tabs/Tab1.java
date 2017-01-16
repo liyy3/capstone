73e9da3d45cee68cc131dfde7cb88c06a6e71f26
Grace Li <graceli1994@hotmail.com>
2017-01-15 22:04:51 -0500

    Milestone B (Jan 15, 2016)

--- app/src/main/java/a40i6_capstone/tabs/Tab1.java
+++ app/src/main/java/a40i6_capstone/tabs/Tab1.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Tab1 extends Fragment{
 
@@ -19,33 +20,33 @@ public class Tab1 extends Fragment{
         View rootView = inflater.inflate(R.layout.tab1contents, container, false);
         //Code to get the button from layout file
         Button btn = (Button) rootView.findViewById(R.id.buttonstart);
+        final TextView startstopmsg = (TextView) rootView.findViewById(R.id.Home_text);
         btn.setTag(0);
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 //Implement the code to run on button click here
-                //((Button) view).setText ("Stop");
                 final int status = (Integer) view.getTag();
                 switch (status) {
                     case 0:
                         ((Button) view).setText("Stop");
+                        startstopmsg.setText("Device is now ON");
                         view.setTag(1); //pause
                         break;
                     case 1:
                         ((Button) view).setText("Start");
+                        startstopmsg.setText("Device is now OFF");
                         view.setTag(0); //pause
                         break;
                 }
+
+
             }
         });
         return rootView;
     }
 
-//    public void TextChange(View view) {
-//        Button startstop = (Button) view;
-//        ((Button) view).setText ("Stop");
-//    }
 
 
 
--- app/src/main/java/a40i6_capstone/tabs/Tab4.java
+++ app/src/main/java/a40i6_capstone/tabs/Tab4.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by grace_000 on 2016-11-22.
@@ -12,10 +13,23 @@ import android.view.ViewGroup;
 
 public class Tab4 extends Fragment {
 
+    private EditText Name1;
+    private EditText Relationship1;
+    private EditText Number1;
+    private EditText Name2;
+    private EditText Relationship2;
+    private EditText Number2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab4contents, container, false);
+
+        Name1 = (EditText) rootView.findViewById(R.id.Name1);
+        Relationship1 = (EditText) rootView.findViewById(R.id.Relationship1);
+        Number1 = (EditText) rootView.findViewById(R.id.Number1);
+        Name2 = (EditText) rootView.findViewById(R.id.Name2);
+        Relationship2 = (EditText) rootView.findViewById(R.id.Relationship2);
+        Number2 = (EditText) rootView.findViewById(R.id.Number2);
 
         return rootView;
     }
