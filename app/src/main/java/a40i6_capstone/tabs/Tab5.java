b1bde507b416e234d1abe214965bc74733fdbc99
Grace Li <graceli1994@hotmail.com>
2017-01-15 22:08:14 -0500

    Milestone B tutorial tab (Jan 15, 2016)

---
+++ app/src/main/java/a40i6_capstone/tabs/Tab5.java
@@ -0,0 +1,24 @@
+package a40i6_capstone.tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by grace_000 on 2017-01-10.
+ */
+
+public class Tab5 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.tab5contents, container, false);
+
+        return rootView;
+    }
+}
