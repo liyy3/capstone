2fee2d9563d0212671ac43579d155b6b1a122b7c
Grace Li <graceli1994@hotmail.com>
2017-02-24 16:58:59 -0500

    Attempt for real time graph (Feb 24, 2017)

--- app/src/main/java/a40i6_capstone/tabs/Tab3.java
+++ app/src/main/java/a40i6_capstone/tabs/Tab3.java
@@ -1,22 +1,90 @@
 package a40i6_capstone.tabs;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Random;
 
 /**
  * Created by grace_000 on 2016-11-19.
  */
 
 public class Tab3 extends Fragment {
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3contents, container, false);
 
+        // we get graph view instance
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(100);
+        //viewport.setScrollable(true);
+
+
         return rootView;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 100; i++) {
+
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // add random data to graph
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 100);
+
+    }
+
+
+
 }
--- app/src/main/java/a40i6_capstone/tabs/Tab4.java
+++ app/src/main/java/a40i6_capstone/tabs/Tab4.java
@@ -1,11 +1,28 @@
 package a40i6_capstone.tabs;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import static a40i6_capstone.tabs.R.layout.tab4contents;
+import static android.R.attr.button;
+import static android.R.id.content;
 
 /**
  * Created by grace_000 on 2016-11-22.
@@ -19,10 +36,12 @@ public class Tab4 extends Fragment {
     private EditText Name2;
     private EditText Relationship2;
     private EditText Number2;
+    private Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab4contents, container, false);
+        View rootView = inflater.inflate(tab4contents, container, false);
 
         Name1 = (EditText) rootView.findViewById(R.id.Name1);
         Relationship1 = (EditText) rootView.findViewById(R.id.Relationship1);
@@ -30,6 +49,21 @@ public class Tab4 extends Fragment {
         Name2 = (EditText) rootView.findViewById(R.id.Name2);
         Relationship2 = (EditText) rootView.findViewById(R.id.Relationship2);
         Number2 = (EditText) rootView.findViewById(R.id.Number2);
+
+        button = (Button) rootView.findViewById(R.id.call_button1);
+        button.setTag(0);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                //int permissionCheck = ContextCompat.checkSelfPermission(this,
+                        //Manifest.permission.WRITE_CALENDAR);
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:16476207478"));
+                    startActivity(callIntent);
+
+            }
+        });
+
 
         return rootView;
     }
